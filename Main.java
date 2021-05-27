3072234d08f88b5a23b90385105cec98cf3e7c2e
MinPt <petr.minailyuk@gmail.com>
2021-05-27 18:20:08 +0300

    Lab-2

--- Main.java
+++ Main.java
@@ -1,158 +1,125 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+package com.Lab2;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Arrays;
-
-class Range{
-    int start, end;
-
-    Range(int start, int end){
-        this.start = Math.min(start,end);
-        this.end = Math.max(start,end);
-    }
-}
+import java.util.HashSet;
 
 public class Main {
-    //27 Створіть метод, який приймає параметр int та конвертує його з десятичної у
-    // восьмирічну систему числення та повертає у вигляді String. Приклад- приймає 9, повертає =011=
-    public static String dexToOct(int decimal){
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        int remainder = 1;
-        int quotient = 0;
-        while(true){
-            remainder = decimal%8;
-            quotient = (int)Math.floor(decimal/8);
-            decimal = (int)Math.floor(decimal/8d);
-            if(quotient == 0){
-
-                arr.add(remainder);
-                break;
-            }
-
-            arr.add(remainder);
-
-        }
-        StringBuilder octal = new StringBuilder();
-        for(int i = arr.size() - 1;i >= 0;i--){
-            octal.append(Integer.toString(arr.get(i)));
-        }
-
-        return octal.toString();
+    public static void main(String[] args) {
+        taskOne();
+        taskTwo();
+        taskThree();
+        taskFour();
     }
 
-    //5 Створіть метод, який дозволяє видаляти всі елементи в масиві int[] ,
-    // які мають певне значення, наприклад дорівнюються 23.
-    // Масив без видалених елементів повинен повертатися, як результат роботи методу
-    public static int[] deleleElems(int[] arr, int elem){
-        ArrayList<Integer> newArray = new ArrayList<Integer>();
-        for (int i = 0;i < arr.length;i++){
-            if(arr[i] == elem){
-                continue;
+    // Продемонструйте у коді  2 способа  обробки виключних ситуацій у методі (з try-catch и  throws),
+    // а також особливості використання блока finally. У якості обробляємо виключення
+    // оберіть будь яке =checked= виключення.
+    public static void taskOne() {
+        try {
+            methodOne();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("Doing smth in try block");
+
+            try {
+                String s = null;
+                s.length();
+            } catch (NullPointerException e) {
+                System.out.println(e.getMessage());
             }
-            newArray.add(arr[i]);
+
+            return;
+        } finally {
+            System.out.println("Finally still executed");
         }
-        int[] returnArray = new int[newArray.size()];
-        for (int i = 0; i < returnArray.length; i++) {
-            returnArray[i] = newArray.get(i);
-        }
-        return returnArray;
     }
 
-    //3 Створіть метод, який дозволяє вставляти в будь яку позицію масиву будь яке число.
-    // Метод повинен повертати новий масив.
-    public static int[] insert(int[] arr, int pos, int elem){
-        if(pos > arr.length){
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        int[] newArray = new int[arr.length + 1];
-        int j = 0, i = 0;
+    private static void methodOne() throws IllegalArgumentException {
+        throw new IllegalArgumentException();
+    }
 
-        while(i < arr.length){
-            if(i == pos && j == i){
-                newArray[j++] = elem;
-                continue;
+    // Продемонструйте у коді генерацію свого виключення AgeStudentException
+    // у конструкторі класу Student при спробі створити об'єкт зі значенням віку помилковим.
+    public static void taskTwo() {
+        try {
+            var student = new Student(1, "Foo");
+        } catch (AgeStudentException e) {
+            System.out.println(e);
+        }
+    }
+
+    // Створіть просту  ієрархію спадкування, яка відповідає сутностям Товар (властивості - назва, виробник, дата виготовлення, ціна)
+    // та Продукти харчування (термін придатності, харчова група (молочні, м'ясні, бакалея та інше)),
+    // РадіоЕлектроніка (вид електроніки, гарантійний термін) Продемонструйте перевантаження конструкторів
+    // та методів, перевизначення методів, особливості роботи конструкторів при спадкуванні.
+    public static void taskThree() {
+        var i = new Item("test",
+                "foo",
+                Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                20f);
+        i.doSmth();
+
+        var e = new Electronics("test",
+                "foo",
+                Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                40f,
+                "bar");
+        e.doSmth();
+        e.doSmth("test");
+
+        var p = new Product("test",
+                "foo",
+                Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                40f,
+                Date.from(LocalDate.now().plusDays(100).atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                "some group");
+    }
+
+    // Створіть 3 різних реалізації інтерфейсу Comparator<String>. Перша реалізація повинна допомагати сортувати
+    // рядки по довжині, друга - по 3 символу, третя- по кількості голосних букв англ. Алфавиту.
+    // Продемонструйте використання усіх default-реалізацій методів цього інтерфейсу.
+    public static void taskFour() {
+        var list = Arrays.asList("cbodsa", "yddz", "aak");
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o2.length(), o1.length());
             }
-            newArray[j++] = arr[i++];
+        });
+
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Character.compare(o1.charAt(3), o2.charAt(3));
+            }
+        });
+
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                var vowelsCount1 = countVowels(o1);
+                var vowelsCount2 = countVowels(o2);
+
+                return Integer.compare(vowelsCount1, vowelsCount2);
+            }
+        });
+    }
+
+    public static int countVowels(String word) {
+        var vowels = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+        var result = 0;
+        for (var c : word.toCharArray()) {
+            result = vowels.contains(c) ? ++result : result;
         }
 
-        return newArray;
-        }
-
-    //22   Створіть метод, який рахує факторіал числа, використовуючи цикли.
-    // Потім створити ще одну реалізацію, яка використовує рекурсію для вирішення цієї задачі
-    public static int factLoop(int number){
-        int result = 1;
-        for (int i = number; i >0 ; i--) {
-            result *= i;
-        }
         return result;
     }
-
-    public static int factRecursive(int number){
-        if(number >= 1){
-            return number* factRecursive(number - 1);
-        }else{
-            return 1;
-        }
-    }
-    //[5,11] && [18,9]
-    public static int[] findIntersuction(int start1,int end1,int start2,int end2){
-
-        Range rang1 = new Range(start1,end1);
-        Range rang2 = new Range(start2,end2);
-        if(rang2.start > rang1.end || rang2.end < rang1.start){
-            return new int[0];
-        }
-        Range intersuction = new Range(Math.max(rang1.start,rang2.start),Math.min(rang1.end, rang2.end));
-        int[] intersucked = new int[intersuction.end - intersuction.start + 1];
-        int j = 0;
-        for (int i = intersuction.start; i <= intersuction.end; i++) {
-            intersucked[j++] = i;
-        }
-        return intersucked;
-    }
-    //[1,2,3]
-    public static int[] findLongestStreak(int[] arr){
-        int i, streak = 1, highestStreak = 1, firstIndex = 0, lastIndex = 0, longestFirstIndex = 0, longestLastIndex = 0;
-        Arrays.sort(arr);
-        for(i = 1;i < arr.length; i++){
-            if(arr[i] == arr[i-1] + 1){
-                streak++;
-                lastIndex = i ;
-                if(streak > highestStreak){
-                    longestFirstIndex = firstIndex;
-                    longestLastIndex = lastIndex;
-                }
-            }else{
-                if(streak > highestStreak){
-                    longestFirstIndex = firstIndex;
-                    longestLastIndex = lastIndex;
-                }
-                streak = 1;
-                firstIndex = i;
-                lastIndex = i;
-            }
-
-            if(streak > highestStreak){
-                highestStreak = streak;
-            }
-        }
-
-        ArrayList<Integer> streakList = new ArrayList<Integer>();
-        for(int j = longestFirstIndex ; j <= longestLastIndex; j++){
-            streakList.add(arr[j]);
-        }
-        int[] streakArray = new int[streakList.size()];
-        for(int j = 0;j < streakList.size();j++){
-            streakArray[j] = streakList.get(j);
-        }
-        return streakArray;
-    }
-    public static void main(String[] args) {
-        int[] arr = {1,3,2,5,7,6,8,9,11,12,99,88,14,15,16,17};
-        int[] streakArray = findLongestStreak(arr);
-        for(int i = 0;i < streakArray.length;i++){
-            System.out.println(streakArray[i]);
-        }
-    }
 }
-
